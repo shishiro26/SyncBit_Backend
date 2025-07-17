4cbada481d94b2d91b63a28bd56d805a557003e6
shishiro26 <bannu1302@gmail.com>
2025-07-17 08:10:40 +0530

    added example routes

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ import { setupSocket } from "./sockets/setupSocket.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.get("/socket-status", (req, res) => {
+  res.json({ status: "Socket server is running" });
+});
 const server = http.createServer(app);
 setupSocket(server);
+
+// Example route
+app.get("/", (req, res) => {
+  res.json({ message: "Hello, world!" });
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
